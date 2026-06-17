fd84b81dcdbee5a033377e68848643744a107fd1
KODALI MANIDEEP <166724881+Mani-2308@users.noreply.github.com>
2026-06-17 11:22:47 +0530

    Implement recursive function to print numbers N to 1

---
+++ 03-basic-recursion/Print-N-to-1-using-recursion.cpp
@@ -0,0 +1,11 @@
+class Solution {
+  public:
+    void printNumbers(int n) {
+        if(n < 1) {
+            return;
+        }
+
+        cout << n << endl;
+        printNumbers(n-1);
+    }
+};
